8cbefd0197f7add1596781fac81ef67161d1a380
Merlin Albes <merlin.albes@gmail.com>
2020-06-11 14:58:51 +0200

    Updates

---
+++ precache-manifest.b8b08b653e186216dc15fdc733916865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392f3ca3be336b98f9b918ca2e4b18fe",
+    "url": "/project202/index.html"
+  },
+  {
+    "revision": "5f923b73c48bf8e08ebd",
+    "url": "/project202/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "848ca29da4a7540b75b9",
+    "url": "/project202/static/js/2.dccea426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project202/static/js/2.dccea426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f923b73c48bf8e08ebd",
+    "url": "/project202/static/js/main.f4d81d1d.chunk.js"
+  },
+  {
+    "revision": "7cf47478b366d49b30c7",
+    "url": "/project202/static/js/runtime-main.0b8206ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project202/static/media/logo.5d5d9eef.svg"
+  }
+]);
